9a99972487791316d0398362ce73ae3c20f7568a
yymin1022 <yymin1022@gmail.com>
2023-02-22 03:05:42 +0900

    Update ListActivity : Added AppBar on Base Component

--- app/src/main/java/com/yong/mt_info/ListActivity.kt
+++ app/src/main/java/com/yong/mt_info/ListActivity.kt
@@ -1,14 +1,14 @@
 package com.yong.mt_info
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import com.yong.mt_info.ui.theme.MountainTheme
 
 class ListActivity : ComponentActivity() {
@@ -29,5 +29,17 @@ class ListActivity : ComponentActivity() {
 
 @Composable
 fun ListUI(name: String) {
-    Text(text = "Hello $name!")
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = { Text("Mountain Finder") }
+            )
+        }
+    ) {
+        Column(
+            modifier = Modifier.padding(it)
+        ) {
+            Text(text = "Hello $name!")
+        }
+    }
 }
