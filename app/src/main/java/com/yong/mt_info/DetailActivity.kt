0f07dffb50f5233429aa5923f2de77d2a113c9f9
yymin1022 <yymin1022@gmail.com>
2023-02-22 02:45:48 +0900

    Update Project : Added DetailActivity

---
+++ app/src/main/java/com/yong/mt_info/DetailActivity.kt
@@ -0,0 +1,43 @@
+package com.yong.mt_info
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.yong.mt_info.ui.theme.MountainTheme
+
+class DetailActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            MountainTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                    Greeting3("Detail Activity")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting3(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview3() {
+    MountainTheme {
+        Greeting3("Detail Activity")
+    }
+}
