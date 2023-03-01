f9afa744d0b3d5746217f267aec25393cc5b8bf1
girinssh <tngus5851@naver.com>
2023-03-01 22:48:40 +0900

    #8 Added Mountain.Kt

---
+++ app/src/main/java/com/yong/mt_info/Mountain.kt
@@ -0,0 +1,15 @@
+package com.yong.mt_info
+
+data class Mountain(
+    val address: String,
+    val admin: String,
+    val adminnum: String,
+    val details: String?,
+    val high: Float,
+    val listno: String,
+    val name: String,
+    val nfdt: String,
+    val sname: String?,
+    val summary: String?,
+    val top: String?
+)
